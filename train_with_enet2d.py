6c38538941d43512c87952a0b496737107d148a9
yuhaonankaka <yuhaonanmessi@hotmail.com>
2020-01-26 00:20:43 +0100

    fix bug

--- train_with_enet2d.py
+++ train_with_enet2d.py
@@ -33,7 +33,7 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
 
-from nlp import LanguageNet, getweights, get_word2idx
+from models.nlp import LanguageNet, getweights, get_word2idx
 from pc_util import write_ply, pyplot_draw_point_cloud, write_ply_rgb
 from utils.projection import ProjectionHelper
 from models.enet import create_enet_for_3d
